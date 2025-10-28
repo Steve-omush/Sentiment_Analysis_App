eed67f1ba4a6c6c6bfbd13a6f383a4bb35fbd86e
Steve-omush <93277164+Steve-omush@users.noreply.github.com>
2025-10-28 21:46:00 +0300

    Determine nltk.txt path dynamically
    Added code to determine the path of nltk.txt relative to the script's location.

--- sentiment_project/download_nltk_data.py
+++ sentiment_project/download_nltk_data.py
@@ -1,4 +1,9 @@
 import nltk
+import os
+
+# Find path of nltk.txt relative to this script
+base_dir = os.path.dirname(__file__)
+nltk_path = os.path.join(base_dir, "nltk.txt")
 
 with open("nltk.txt") as f:
     packages = f.read().splitlines()
